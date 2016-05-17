1d70655631bf4d23717f9c56b2671ea25ef29b91
Bombebak <Den_Farlige_Pilot@hotmail.com>
2016-05-17 11:14:06 +0200

    first commit

---
+++ SquareApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SquareApplication.Startup))]
+namespace SquareApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
