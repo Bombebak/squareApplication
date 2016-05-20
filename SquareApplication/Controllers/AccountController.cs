534263d1029b741cf30466f61e48411853f55788
David Kelemen <david.nkelemen@gmail.com>
2016-05-20 06:10:05 +0200

    Revert "Revert "Register""
    This reverts commit 1259ddf83af1e752a79f18572abf558f3400d3f8.

--- SquareApplication/Controllers/AccountController.cs
+++ SquareApplication/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
@@ -151,25 +152,27 @@ namespace SquareApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
+                var user = new Models.UserViewModel
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    Address = model.Address, Email = model.Email, Name = model.Name,
+                    Password = model.Password, isDesigner = model.Designer
+                };
+                using (SquaresEntities dbContext = new SquaresEntities())
+                {
+                    dbContext.Users.Add(new User
+                    {
+                         address = user.Address,
+                         email = user.Email,
+                         isDesigner = user.isDesigner,
+                         name = user.Name,
+                         password = user.Password,
+                    });
+                    dbContext.SaveChanges();
                 }
-                AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
-            return View(model);
+            return RedirectToAction("Index", "Home");
         }
 
         //
