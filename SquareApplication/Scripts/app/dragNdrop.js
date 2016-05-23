0813c64a26ad432cb41faa519907d2dbea64acf7
codobbit <alice.puricica@gmail.com>
2016-05-23 18:42:17 +0200

    Some tweaks in the css

--- SquareApplication/Scripts/app/dragNdrop.js
+++ SquareApplication/Scripts/app/dragNdrop.js
@@ -17,9 +17,7 @@
         }
 
     function drop(ev) {
-        //TODO:scale down every pic
-        //TODO:duplicate all images
-        ev.preventDefault();
+              ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         var parent=findAncestor(ev.target,"productGridItem");
         if (parent == null)
@@ -33,7 +31,10 @@
             //TODO: empty parent and add the new tile
             parent.innerHTML="";
             parent.appendChild(document.getElementById(data));
-
+        }
+          if ($('#selectedTileStyle').is(':empty')){
+            var placeholder = $('<img class="previewImage" src="http://placehold.it/299x299"/>');
+            $('#selectedTileStyle').append(placeholder);
         }
     }
 //find the ancestor of the image
